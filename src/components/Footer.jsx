f647ffdb263fa07aed1081d0cd1f08140700b52b
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-07-29 15:31:55 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+export default function Footer(){
+
+return(
+
+<footer
+
+style={{
+
+padding:"50px 0",
+background:"#0f172a",
+color:"white",
+marginTop:"100px"
+
+}}
+
+>
+
+<div className="container">
+
+<h2>
+
+Explore IT Consulting
+
+</h2>
+
+<p>
+
+© 2026 Explore IT Consulting
+
+</p>
+
+</div>
+
+</footer>
+
+)
+
+}
