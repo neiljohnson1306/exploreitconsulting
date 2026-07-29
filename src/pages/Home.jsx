9fa5377cebea879069c553a7d3e580e8f4435d2f
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-07-29 15:49:55 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,74 +1,13 @@
-export default function Home(){
+import Hero from "../components/Hero";
+import Stats from "../components/Stats";
+import TrustedCompanies from "../components/TrustedCompanies";
 
-    return(
-
-        <>
-        <section className="section">
-
-        <div className="container">
-
-        <h1
-        style={{
-            fontSize:"70px",
-            lineHeight:"1.1",
-            fontWeight:"800"
-        }}
-        >
-
-        Empowering Businesses
-        <br/>
-
-        <span className="gradient-text">
-        Through Technology
-        </span>
-
-        </h1>
-
-        <p
-        style={{
-            maxWidth:700,
-            fontSize:"22px",
-            marginTop:30,
-            color:"#64748b"
-        }}
-        >
-
-        Explore IT Consulting helps organisations modernise,
-        innovate and scale using cloud technologies,
-        software engineering,
-        AI,
-        business analysis and digital transformation.
-
-        </p>
-
-        <div
-        style={{
-            display:"flex",
-            gap:20,
-            marginTop:40
-        }}
-        >
-
-        <a href="#services" className="btn-primary">
-
-        Book Consultation
-
-        </a>
-
-        <a href="#services" className="btn-outline">
-
-        Our Services
-
-        </a>
-
-        </div>
-
-        </div>
-
-        </section>
-
-        </>
-
-    )
-
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Stats />
+      <TrustedCompanies />
+    </>
+  );
 }
