08d58fad07315ae1763db19bb44bc3f0c4ff56e9
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-07-29 15:47:27 +0100

    Create stats.js

---
+++ src/data/stats.js
@@ -0,0 +1,18 @@
+export const stats = [
+  {
+    number: "300+",
+    title: "Projects Delivered"
+  },
+  {
+    number: "50+",
+    title: "Enterprise Clients"
+  },
+  {
+    number: "12+",
+    title: "Years Experience"
+  },
+  {
+    number: "99%",
+    title: "Client Satisfaction"
+  }
+];
