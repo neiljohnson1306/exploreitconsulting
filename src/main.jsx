4efea1c8a40e62b56116c1d57099659c71a64681
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-07-29 16:02:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
