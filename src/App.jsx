83052f37536cfcba857dd38c1bd11062dc026ff5
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-07-29 15:32:11 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+
+export default function App(){
+
+return(
+
+<Layout>
+
+<Routes>
+
+<Route
+path="/"
+element={<Home/>}
+/>
+
+</Routes>
+
+</Layout>
+
+)
+
+}
